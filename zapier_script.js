289eca75d428b2ce38c2f1f2ad60d967a3a7065c
vincepaw <vincep@advisorwebsites.com>
2018-11-19 16:58:50 -0800

    address formatting per Lucas

--- zapier_script.js
+++ zapier_script.js
@@ -10,23 +10,25 @@ return {
 				    +	'"hubspot_deal_id": "'+ inputData.hupspot_deal_id + '", '
 				    +	'"hupspot_id": "'     + inputData.hubspot_id      + '"  ' // removed comma for last element
 				
-					/********Address********
 				 
 					+ '"address": ['   
-					+   '{'		//If Country is US
-					+		   '"country": "'+ inputData.country+ '",' 
-					+          '"street": "' + inputData.street + '",' 
-					+          '"zip": "'    + inputData.zip    + '",'
-					+   '},'
-					+   '{'		//If Country is CA
-					+		   '"country": "'+ inputData.country+ '",'
+					+   '{'	
+					+		   '"country_code": "'+ inputData.country+ '",'
+                    +          '"langcode": "'    + /*TODO*/ +         '",'
+                    +          '"given_name": "'  + /*TODO*/ +         '",'
+                    +          '"family_name": "' + /*TODO*/ +         '",'
+                    +          '"organization": "' +/*TODO*/ +         '",'
+                    +          '"address_line1": "'+/*TODO*/ +         '",'
+                    +          '"address_line2": "'+/*TODO*/ +         '",'
+                    +          '"locality": "'     +/*TODO*/ +         '",'
+                    +          '"admistrative_area": "' + /*TODO*/ +   '",'
+                    +          '"postal_code": "' + /*TODO*/ +         '",'
 					+		   '"street": "' + inputData.street + '",'
 					+		   '"postal": "' + inputData.postal + '",'
 
 
 					+    '}'
 					+  ']'
-					**********************/
 					
 					
 					
