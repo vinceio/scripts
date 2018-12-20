9f18a4c4e7ab268a28d1a59ffb9672eb382dc4fe
vincepaw <vincep@advisorwebsites.com>
2018-12-20 11:15:20 -0800

    updates to country code and province/state lookups

--- zapier_script.js
+++ zapier_script.js
@@ -1,8 +1,24 @@
-var country = "CA";
+//Lookup Values for States and Provinces (aka. Administrative Area)
+const states = {"- Select -":"","Alabama":"AL","Alaska":"AK","American Samoa":"AS","Arizona":"AZ","Arkansas":"AR","Armed Forces (AA)":"AA","Armed Forces (AE)":"AE","Armed Forces (AP)":"AP","California":"CA","Colorado":"CO","Connecticut":"CT","Delaware":"DE","District of Columbia":"DC","Florida":"FL","Georgia":"GA","Guam":"GU","Hawaii":"HI","Idaho":"ID","Illinois":"IL","Indiana":"IN","Iowa":"IA","Kansas":"KS","Kentucky":"KY","Louisiana":"LA","Maine":"ME","Marshall Islands":"MH","Maryland":"MD","Massachusetts":"MA","Michigan":"MI","Micronesia":"FM","Minnesota":"MN","Mississippi":"MS","Missouri":"MO","Montana":"MT","Nebraska":"NE","Nevada":"NV","New Hampshire":"NH","New Jersey":"NJ","New Mexico":"NM","New York":"NY","North Carolina":"NC","North Dakota":"ND","Northern Mariana Islands":"MP","Ohio":"OH","Oklahoma":"OK","Oregon":"OR","Palau":"PW","Pennsylvania":"PA","Puerto Rico":"PR","Rhode Island":"RI","South Carolina":"SC","South Dakota":"SD","Tennessee":"TN","Texas":"TX","Utah":"UT","Vermont":"VT","Virgin Islands":"VI","Virginia":"VA","Washington":"WA","West Virginia":"WV","Wisconsin":"WI","Wyoming":"WY"};
+
+const provinces = {"- Select -":"","Alberta":"AB","British Columbia":"BC","Manitoba":"MB","New Brunswick":"NB","Newfoundland and Labrador":"NL","Northwest Territories":"NT","Nova Scotia":"NS","Nunavut":"NU","Ontario":"ON","Prince Edward Island":"PE","Quebec":"QC","Saskatchewan":"SK","Yukon":"YT"};
+
+
+let adminareaCode = '';
+
+
+if (states.hasOwnProperty(inputData.administrative_area)){
+		adminareaCode = states[inputData.administrative_area];
+}
+
+if (provinces.hasOwnProperty(inputData.administrative_area)){
+		adminareaCode = province[inputData.administrative_area];
+}
 
 let hubspotInfo = {};
-let org = inputData.organization.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
+//removes special characters then lower cases organization string
+let org = inputData.organization.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
 
 hubspotInfo.id = org;
 hubspotInfo.label = org;
@@ -17,11 +33,9 @@ for (var i = firstLevelOptionals.length - 1; i >= 0; i--) {
         hubspotInfo[firstLevelOptionals[i]] = inputData[firstLevelOptionals[i]];
     }
 }
-const addressOptions = ['langcode', 'given_name', 'family_name', 'organization', 'address_line1', 'locality', 'administrative_area', 'postal_code'];
+const addressOptions = ['langcode', 'given_name', 'family_name', 'organization', 'address_line1', 'locality', 'postal_code'];
 
 let address = {};
-
-
 
 for (var i = addressOptions.length - 1; i >= 0; i--) {
     if (inputData.hasOwnProperty(addressOptions[i])) {
@@ -29,25 +43,22 @@ for (var i = addressOptions.length - 1; i >= 0; i--) {
     }
 }
 
-let countryCode = '';
-if (inputData.hasOwnProperty('country')) {
-    switch (inputData.country) {
-        case 'Canada':
-            countryCode = 'CA';
-        default:
-            countryCode = 'US';
-    }
+
+//Checks for Country Code
+if (inputData.hasOwnProperty('country') && ((inputData.country == 'CA') || (inputData.country == 'US'))) {
+		      address.country_code = inputData.country;
 }
 
-if (countryCode.length) {
-    address.country_code = countryCode;
+if (adminareaCode.length){
+		address.administrative_area = adminareaCode;
 }
 
 hubspotInfo.address = address;
 
+//Convert hubspotInfo into JSON formatted string
 const exportJson = JSON.stringify(hubspotInfo);
 
-
+//Create Base64 encoded string then encoded as Uniform Resource Identifier(URI)
 return {
     string: 'https://lucaccounts.force1.awdev.ca/site-creation?i='
             +encodeURIComponent(
